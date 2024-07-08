198520ad244c42ef4f2b6000982f2438f89a16d6
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-08 16:05:14 +0300

    add template strings way

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,49 +1,25 @@
-let radius = 10;
-let pi = 3.14;
+// template strings
+const title = 'best reads of 2019';
+const author = 'mario';
+const likes = 30;
 
-// console.log(radius,pi);
-// 10 3.14
-
-// math operators +, -, *, /, **. %
-
-// console.log(10/2);
-// 5
-
-// let result = radius % 3;
-// 1
-
-// let result = pi * radius**2;
-// 314
-
-// order of operation - B I D M A S
-// B I D M A S = brackets - indices - division - multiplication - addition - subtraction
-
-// let result = 5 * (10-3)**2;
-// console.log(result);
-// 245
+// concattenation way
+// let result = 'the blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 
 
+// the blog called best reads of 2019 by mario has 30 likes
 
-let likes = 10;
-// likes ++
-// 11
-// likes --
-// 9
-// likes += 10;
-// 20
-// likes -= 5 ;
-// 5
-// likes *= 2 ;
-// 20
-// likes /= 2 ;
-// 5
-
-// console.log(likes);
-
-// NaN - not a number
-
-// console.log(10/'karim');
-
-let result = 'this is first ' + likes + ' numbers in our likes';
-// this is first 10 numbers in our likes
+// template string way
+let result = `the blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+// the blog called best reads of 2019 by mario has 30 likes
+
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>${author}</p>
+    <span> this blog has ${likes} likes </span>
+`;
+
+console.log(html);
