74f191b3932521b0a34d3fb813069533909540e9
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-08 17:22:26 +0300

    loose comparison and strict comparison

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,38 +1,24 @@
-console.log(true,false, 'true', 'false');
-
-// methods can return Booleens
-
-let email = 'karim@gmail.com';
-// let names = ['matio','luigi','toad'];
-
-// let result = email.includes('@');
-// true
-// let result = names.includes('uigi');
-// false
-// console.log(result);
-
-// comparison operators
 let age = 25;
+
+// loose comparison (different types can still be equal)
 
 console.log(age == 25);
 // true
-console.log(age == 30);
+console.log(age == '25');
+// true
+console.log(age != 25);
 // false
-console.log(age != 20); // != is age not equal 25
-// true
-console.log(age > 20);
-// true
-console.log(age < 20);
+console.log(age != '25');
 // false
-console.log(age <= 25);
-// true
-console.log(age >= 25);
-// true
 
-let names = 'shaun';
 
-console.log(names == 'shaun');
-console.log(names == 'Shaun');
-console.log(names > 'crystal');
-console.log(names > 'Shaun'); // lowercase is greater than uppercase
-console.log(names > 'Crystal');
+// strict comparison (different types cannot be equal)
+
+console.log(age === 25);
+// true
+console.log(age === '25');
+// false
+console.log(age !== 25);
+// false
+console.log(age !== '25');
+// true
