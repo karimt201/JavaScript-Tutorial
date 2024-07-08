6ca84fc4b3d1d0ee9c16a97575571879eb47afb0
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-08 15:31:51 +0300

    add Numbers and math operators

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,18 +1,49 @@
-// common String methods
+let radius = 10;
+let pi = 3.14;
 
-let email = 'karim@gmail.com';
+// console.log(radius,pi);
+// 10 3.14
 
-// let result = email.lastIndexOf('l');
-// 10
+// math operators +, -, *, /, **. %
 
-// let result = email.slice(0,5);
-// karim
+// console.log(10/2);
+// 5
 
-// let result = email.substr(5,5);
-// @gmai
+// let result = radius % 3;
+// 1
 
-let result = email.replace('i','e');
-// karem@gmail.com
+// let result = pi * radius**2;
+// 314
+
+// order of operation - B I D M A S
+// B I D M A S = brackets - indices - division - multiplication - addition - subtraction
+
+// let result = 5 * (10-3)**2;
+// console.log(result);
+// 245
 
 
+
+let likes = 10;
+// likes ++
+// 11
+// likes --
+// 9
+// likes += 10;
+// 20
+// likes -= 5 ;
+// 5
+// likes *= 2 ;
+// 20
+// likes /= 2 ;
+// 5
+
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(10/'karim');
+
+let result = 'this is first ' + likes + ' numbers in our likes';
+// this is first 10 numbers in our likes
 console.log(result);
