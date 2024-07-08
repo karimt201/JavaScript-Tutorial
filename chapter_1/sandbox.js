85155f80ee8a425f0d9a3bdeab7d54c95ec732fe
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-08 17:12:59 +0300

    add methods can return Booleens

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,7 +1,38 @@
-// let age ;
-// undefined NaN 'the age is undefined'
-let age = null;
-// null 3 'the age is null'
+console.log(true,false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return Booleens
 
+let email = 'karim@gmail.com';
+// let names = ['matio','luigi','toad'];
+
+// let result = email.includes('@');
+// true
+// let result = names.includes('uigi');
+// false
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+console.log(age == 25);
+// true
+console.log(age == 30);
+// false
+console.log(age != 20); // != is age not equal 25
+// true
+console.log(age > 20);
+// true
+console.log(age < 20);
+// false
+console.log(age <= 25);
+// true
+console.log(age >= 25);
+// true
+
+let names = 'shaun';
+
+console.log(names == 'shaun');
+console.log(names == 'Shaun');
+console.log(names > 'crystal');
+console.log(names > 'Shaun'); // lowercase is greater than uppercase
+console.log(names > 'Crystal');
