f5eb369d89ac688742a1aba50c3dbae909d02893
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-08 16:52:49 +0300

    add undefined and null

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,33 +1,7 @@
-let ninja = ['shaun' , 'ryu' , 'chun-li'];
+// let age ;
+// undefined NaN 'the age is undefined'
+let age = null;
+// null 3 'the age is null'
 
-// ninja[1]='ken'
-// console.log(ninja[1]);
-// 'ryu'
-
-// let ages = [20,25,30,35];
-// console.log(ages[0]);
-// 20
-
-// let random = ['shaun' , 'crystal' , 20, 30];
-
-// console.log(random[3]);
-// 30
-
-// console.log(ninja.length);
-// 3
-
-// array methods
-
-// let result = ninja.join('-');
-// shaun-ryu-chun-li
-// let result = ninja.indexOf('ryu');
-// (3)
-// let result = ninja.concat(['ken','crystal']);
-// (5) ['shaun', 'ryu', 'chun-li', 'ken', 'crystal']
-// let result = ninja.push('ken')
-// (4) ['shaun', 'ryu', 'chun-li', 'ken']
-let result = ninja.pop();
-//  ['shaun', 'ryu']
-console.log(result);
-console.log(ninja);
+console.log(age, age + 3, `the age is ${age}`);
 
