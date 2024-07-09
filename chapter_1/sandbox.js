83c2274f0cedde2cdc6663a5d6b30a400d67360b
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-09 20:58:19 +0300

    while loop

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,15 +1,15 @@
-// for loop
+// while loop
 
-// for(let i = 3 ; i < 5 ; i++){
+
+// let i = 0;
+// while(i < 5){
 //     console.log('in loop', i);
+//     i++;
 // }
 
-// console.log('loop finished');
-
 const names = ['shaun','mario','luigi'];
-
-for(let i =0 ; i < names.length ; i++){
+let i = 0;
+while(i<names.length){
     console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+    i++
 }
