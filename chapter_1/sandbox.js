6d84f681eb0729e3d2d7c383e09cbe13fe74fe7f
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-10 13:44:52 +0300

    regular and Arrow functions

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,17 +1,46 @@
-//  returning values
+// regular functions
 
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-    
-};
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// };
 
+//  Arrow Function
+
+const calcArea = radius => 3.14 * radius**2 ;
 
 const area = calcArea(5);
-console.log(area);
+console.log(`this area is : ${area}`);
 
-const clacval = function(area){
+// practise Arrow Function
 
-};
+// regular functions
 
-const s = clacval(area);
-console.log(s);
+// const greet = function(){
+//     return 'hallo, world'
+// };
+
+//  Arrow Function
+
+const greet = () => 'hallo, world';
+
+const test = greet();
+console.log(test);
+
+// const bill = function (products , tax) {
+//     let total = 0 ;
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// };
+
+const bill = (products,tax) => {
+    let total = 0 ;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+const result = bill([1,5,3],0.5);
+console.log(result);
