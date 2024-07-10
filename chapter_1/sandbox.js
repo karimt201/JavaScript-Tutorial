9b2f3ef2b92a180850596990b792f4eb3eabd667
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-10 14:11:13 +0300

    functions and methods

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,46 +1,12 @@
-// regular functions
+const name = 'shaun';
 
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
-// };
+// functions
 
-//  Arrow Function
+const greet = () => 'hello';
 
-const calcArea = radius => 3.14 * radius**2 ;
+let resultOne = greet();
+console.log(resultOne);
 
-const area = calcArea(5);
-console.log(`this area is : ${area}`);
-
-// practise Arrow Function
-
-// regular functions
-
-// const greet = function(){
-//     return 'hallo, world'
-// };
-
-//  Arrow Function
-
-const greet = () => 'hallo, world';
-
-const test = greet();
-console.log(test);
-
-// const bill = function (products , tax) {
-//     let total = 0 ;
-//     for (let i = 0; i < products.length; i++) {
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// };
-
-const bill = (products,tax) => {
-    let total = 0 ;
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-}
-
-const result = bill([1,5,3],0.5);
-console.log(result);
+// methods
+let resultTwo = name.toUpperCase()
+console.log(resultTwo);
