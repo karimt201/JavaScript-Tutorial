17265a965d4ba2a9a19a7e7d3fc7b8c589bcf0c9
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-10 12:36:29 +0300

    function declaration and expression

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,23 @@
+
+// function expression
+
+const speak = function() {
+  console.log('good day!');  
+};
+
+
+greet();
+greet();
+greet();
+
+
+speak();
+speak();
+speak();
+
+
+// function declaration
+
+function greet() {
+    console.log('hello there');
+}
