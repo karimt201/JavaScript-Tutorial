45036bbd067d6d2196458a459c7297502b8389da
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-14 09:12:20 +0300

    querySelectorAll

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,13 @@
-const para = document.querySelector('body > h1');
+// const para = document.querySelector('body > h1');
 
-console.log(para);
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+// paras.forEach(h => {
+//     console.log(h);
+// });
+
+console.log(paras[0]);
+
+console.log(errors[1]);
