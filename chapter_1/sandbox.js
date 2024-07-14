1d87c505121b7be511be4178bb0e3a97c3e7f113
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-14 21:36:09 +0300

    add and remove style Attributes

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,16 +1,7 @@
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
-link.setAttribute('href','https//www.facebook.com');
-link.innerText = 'the facebook website ....';
-console.log(link.getAttribute('href'));
+const title = document.querySelector('h1');
 
+// title.setAttribute('style','margin: 50px;');
 
-const para = document.querySelector('p');
-console.log(para.getAttribute('class'));
-
-para.setAttribute('class','success');
-console.log(para.getAttribute('class'));
-
-para.setAttribute('style','color:green;');
-console.log(para.getAttribute('style'));
-
+title.style.margin = '';
+title.style.color = 'crimson';
+title.style.fontSize = '40px';
