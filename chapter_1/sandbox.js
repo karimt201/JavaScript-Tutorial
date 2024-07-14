105876f50a4b4c8576dab649e22f661d5c2054f2
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-07-14 10:37:22 +0300

    getAttribute and setAttribute

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,23 +1,16 @@
+const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+link.setAttribute('href','https//www.facebook.com');
+link.innerText = 'the facebook website ....';
+console.log(link.getAttribute('href'));
+
+
 const para = document.querySelector('p');
-console.log(para.innerText);
-para.innerText = 'hello spain';
+console.log(para.getAttribute('class'));
 
-const paras = document.querySelectorAll('p');
+para.setAttribute('class','success');
+console.log(para.getAttribute('class'));
 
-// paras.forEach(h =>{
-//     console.log(h.innerText);
-//     h.innerText += ' new text';
-// });
+para.setAttribute('style','color:green;');
+console.log(para.getAttribute('style'));
 
-const content = document.querySelector('.content');
-// console.log(content.innerHTML);
-
-content.innerHTML += '<h2>this is a new h2 </h2>';
-console.log(content.innerHTML);
-
-const people = ['mario','luigi','yoshi'];
-
-people.forEach(person => {
-    content.innerHTML += `<p> this is ${person}</p>`;
-   
-});
